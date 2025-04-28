0082f1a9069c0cfdeba169e9f2b355937bbc00f3
voirindev <yves.voirin@geoimagesolutions.com>
2025-04-28 14:30:14 -0400

    Add initial project files: devcontainer configuration, library imports, and requirements

---
+++ checklibs.py
@@ -0,0 +1,9 @@
+import geopandas as gpd
+import rasterio
+import numpy as np
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import lxml
+
+print("Toutes les bibliothèques ont été importées avec succès ! 🎉")
